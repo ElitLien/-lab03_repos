ec8c0c03267306d2f62ae78488a70036d1ff9302
ElitLien <supervlad586@gmail.com>
2020-10-18 22:20:40 +0300

    first branch_034 commit

---
+++ PR3.4/PR3.4/PR3.4.cpp
@@ -0,0 +1,25 @@
+// Lab_03_4.cpp
+// М'ягких Владислав
+// Лабораторна робота № 3.4
+// Розгалуження, задане плоскою фігурою.
+// Варіант 11
+#include <iostream>
+using namespace std;
+int main()
+{
+	double x; 
+	double y; 
+	double R;
+
+	cout << "x = "; cin >> x;
+	cout << "y = "; cin >> y;
+	cout << "R = "; cin >> R;
+
+	if ((y <= sqrt((R * R) - (x * x)) && x >= 0 && y>=0) ||
+		(y >= - x - R && y <= 0 && x <=0))
+		cout << "yes" << endl;
+	else
+		cout << "no" << endl;
+	cin.get();
+	return 0;
+}
